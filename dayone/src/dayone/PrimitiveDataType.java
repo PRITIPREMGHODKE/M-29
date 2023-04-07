499ae423ab44bb8394e0f7a4d86506535f3c9439
Admin <Admin@Admin-PC>
2023-04-07 13:30:18 +0530

    First Commit

---
+++ dayone/src/dayone/PrimitiveDataType.java
@@ -0,0 +1,19 @@
+package dayone;
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		byte bytemax=127;
+		short shortmax=32764;
+		long var2=8437536087435L;
+		char varl='V';
+		boolean flag=true;
+		System.out.println(bytemax);
+		System.out.println(shortmax);
+		System.out.println(var2);
+		System.out.println("character is:"+varl);
+		System.out.println("Flag is: "+flag);
+
+	}
+
+}
