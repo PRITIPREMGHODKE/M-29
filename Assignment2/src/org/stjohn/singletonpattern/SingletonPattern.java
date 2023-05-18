3cd3fbc93a9688813c4f1ee83d5740aff1d21917
Admin <Admin@Admin-PC>
2023-05-18 15:18:38 +0530

    assignment2 commit

---
+++ Assignment2/src/org/stjohn/singletonpattern/SingletonPattern.java
@@ -0,0 +1,15 @@
+//program on demonstrate to singleton pattern using eager initalization
+package org.stjohn.singletonpattern;
+public class SingletonPattern {
+	//create private constructor
+		private static EagerPattern INSTANCE = new EagerPattern();
+		
+		//create private static variable of the same class
+		private EagerPattern() {
+			
+		}
+		//create public static method that returns instance of the class
+		public static EagerPattern getInstance() {
+			return INSTANCE;
+		}
+}
