a89762070d0584bf47290d69ed4876a08e44bb8e
Admin <Admin@Admin-PC>
2023-06-12 14:05:20 +0530

    twelve commit

---
+++ daytwelvee/src/org/stjohn/collection/set/LinkedHashSetDemo.java
@@ -0,0 +1,18 @@
+package org.stjohn.collection.set;
+import java.util.LinkedHashSet;
+public class LinkedHashSetDemo {
+
+	public static void main(String[] args) {
+		LinkedHashSet<String> lhs=new LinkedHashSet<String>();
+		lhs.add("Neha");
+		lhs.add("Sakshi");
+		lhs.add("Shrutika");
+		lhs.add("Purvi");
+		lhs.add("Rajashree");
+		lhs.add("Purvi");
+		lhs.add(null);
+		System.out.println(lhs);
+
+	}
+
+}
---
+++ daytwelvee/src/org/stjohn/list/ArrayListDem.java
@@ -0,0 +1,23 @@
+package org.stjohn.list;
+import java.util.ArrayList;
+import com.tns.threading.Person;
+public class ArrayListDem {
+
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList(); // Raw list - Heterogenous
+		list.add(10);
+		list.add(40);
+		list.add(34.78);
+		list.add(3400888l);
+		list.add(true);
+		list.add('A');
+		list.add("Nashik");
+		Person p = new Person();
+		p.setId(101);
+		p.setName("Nisha");
+		list.add(p);
+		System.out.println(list);
+
+	}
+
+}
