62de358105d30c2bfbf4cc415daf137947ad1228
Admin <Admin@Admin-PC>
2023-06-16 12:47:34 +0530

    first commit

---
+++ MoneyMoneyBank/src/org/stjohn/client/MoneyMoneyBankClient.java
@@ -0,0 +1,34 @@
+//Driver class
+//Program to demonstrate on MoneyMoneyBank App
+package org.stjohn.client;
+import org.sjcem.application.MMBankFactory;
+import org.sjcem.application.MMCurrentAcc;
+import org.sjcem.application.MMSavingAcc;
+import org.sjcem.framework.BankFactory;
+import org.sjcem.framework.CurrentAcc;
+import org.sjcem.framework.SavingAcc;
+public class MoneyMoneyBankClient {
+	public static void main(String[] args) {
+		BankFactory b = new MMBankFactory();
+		SavingAcc s = new MMSavingAcc(40005, "priti ghodke", 2000, true);
+		CurrentAcc c = new MMCurrentAcc(50005, "paras ghodke", 5500, 500);
+		
+		System.out.println("Saving Account");
+		s.withdraw(500);
+		
+		System.out.println();
+		
+		System.out.println("Current Account");
+		c.withdraw(200);
+		
+		System.out.println();
+		
+		
+		System.out.println(s);
+		System.out.println(c);
+	
+
+		
+		
+	}
+}
---
+++ MoneyMoneyBank/src/org/stjohn/framework/BankFactory.java
@@ -0,0 +1,6 @@
+package org.stjohn.framework;
+
+public abstract class BankFactory {
+	abstract public SavingAcc getNewSavingAcc(int AccNo, String accNm, float accBal, boolean isSalaried);
+	abstract public CurrentAcc getNewCurrentAcc(int AccNo, String accNm, float accBal, float creditLimit);
+}
